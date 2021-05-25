9e1b582d69297d17a87a29e3d44c069bce016dda
detalikota <detalikota@gmail.com>
2021-05-25 08:23:43 +0300

    yeah

--- main/models.py
+++ main/models.py
@@ -1,6 +1,12 @@
 from django.core.exceptions import DisallowedHost
-from django.db import models
-
+from django.db import DefaultConnectionProxy, models
+   
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    def __str__(self):
+        return self.username
 class Trades(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coin = models.CharField(max_length=10)
+    profilePicture = models.ImageField(default='default.jpg', upload_to='profile_pics')
     
